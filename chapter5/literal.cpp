2bdd8e908b79c4ca22eb8f36b2867900ab208a89
MS_D_rei <msmdmission1111@gmail.com>
2023-12-12 17:48:06 +0900

    Create: chapter5/literal.cpp

---
+++ chapter5/literal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Literals are values that are inserted directly into the code.
+
+int main() {
+    bool isTrue {true}; // true is a boolean literal
+    double d {1.2}; // 1.2 is a double literal
+    std::cout << "isTrue: " << isTrue << std::endl; // "isTrue: 1" is a C-style string literal
+}
