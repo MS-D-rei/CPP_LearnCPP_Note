1c563a37db6451048479cd8ad2cd09e97f4fb5ae
MS_D_rei <msmdmission1111@gmail.com>
2024-01-18 21:57:53 +0900

    Create: chapter14/constructor.cpp

---
+++ chapter14/constructor.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+class Yogurt {
+   public:
+    // constructor
+    // 1. same name as the class.
+    // 2. constructor dont't make any object.
+    // compiler will make an object before the constructor is called.
+    // and then the constructor is called on the uninitialized object.
+    // 3. constructor should not be const.
+    Yogurt(std::string_view flavor) { setFlavor(flavor); }
+
+    void setFlavor(std::string_view flavor) { m_flavor = flavor; }
+
+    std::string_view getFlavor() const { return m_flavor; }
+
+   private:
+    std::string m_flavor{};
+};
+
+void printYogurt(const Yogurt& yogurt) {
+    std::cout << "Yogurt(" << yogurt.getFlavor() << ")\n";
+}
+
+int main() {
+    Yogurt yogurt1{"strawberry"};
+    printYogurt(yogurt1);
+
+    const Yogurt vanillaYogurt{"vanilla"};
+    printYogurt(vanillaYogurt);
+
+    return 0;
+}
