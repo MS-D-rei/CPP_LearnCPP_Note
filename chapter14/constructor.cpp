a42197b34e3e79cf045eb7afe464132480a46ff6
MS_D_rei <msmdmission1111@gmail.com>
2024-01-18 23:06:27 +0900

    Modify: constructor func with member initializer list
    in chapter14/constructor.cpp

--- chapter14/constructor.cpp
+++ chapter14/constructor.cpp
@@ -10,7 +10,11 @@ class Yogurt {
     // compiler will make an object before the constructor is called.
     // and then the constructor is called on the uninitialized object.
     // 3. constructor should not be const.
-    Yogurt(std::string_view flavor) { setFlavor(flavor); }
+    Yogurt(std::string_view flavor)
+        // Member variables in a member initializer list should be listed in
+        // order that they are defined in the class.
+        : m_flavor{flavor}  // member initializer list
+    {}
 
     void setFlavor(std::string_view flavor) { m_flavor = flavor; }
 
