bc85f0c7710ac19360a5862dc830af05a499f828
MS_D_rei <msmdmission1111@gmail.com>
2024-01-19 16:37:06 +0900

    Create: chapter14/delegating.cpp

---
+++ chapter14/delegating.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+// If you have multiple constructors, consider whether you can use delegating
+// constructors to reduce duplicate code.
+
+class Employee {
+   private:
+    std::string m_name{};
+    int m_id{};
+
+   public:
+    // delegating constructor
+    // a constructor that delegates to another constructor is not allowed to do
+    // any member initialization itself.
+    Employee(std::string_view name) : Employee{name, 0} {}
+    // It is common to have the constructor with fewer parameters delegate to
+    // the constructor with more parameters.
+    Employee(std::string_view name, int id) : m_name{name}, m_id{id} {
+        std::cout << "Employee " << m_name << " created.\n";
+    }
+};
+
+int main() {
+    Employee alex{"Alex"};
+    Employee joe{"Joe", 42};
+
+    return 0;
+}
