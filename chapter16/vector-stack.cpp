9b2846e6f9921cc39ec49c3c5a42deda1c08fb10
MS_D_rei <msmdmission1111@gmail.com>
2024-01-27 14:43:43 +0900

    Create: chapter16/vector-stack.cpp

---
+++ chapter16/vector-stack.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void printStack(const std::vector<T>& stack) {
+    if (stack.empty()) {
+        std::cout << "stack is empty\n";
+    }
+
+    for (const auto& elem : stack) {
+        std::cout << elem << ' ';
+    }
+    std::cout << '\n';
+
+    std::cout << "size: " << stack.size() << '\n';
+    std::cout << "capacity: " << stack.capacity() << '\n';
+    std::cout << '\n';
+}
+
+int main() {
+    std::vector<int> stack{};
+    printStack(stack);
+
+    // reserve() changes the capacity of the vector.
+    // It does not change the size of the vector.
+    // resize() changes the size of the vector.
+    stack.reserve(6);
+    printStack(stack);
+
+    // push_back() adds an element to the end of the vector.
+    stack.push_back(1);
+    printStack(stack);
+
+    stack.push_back(2);
+    printStack(stack);
+
+    stack.push_back(3);
+    printStack(stack);
+
+    std::cout << "top: " << stack.back() << '\n';
+    std::cout << '\n';
+
+    // pop_back() removes the last element of the vector.
+    stack.pop_back();
+    printStack(stack);
+
+    stack.pop_back();
+    printStack(stack);
+
+    stack.pop_back();
+    printStack(stack);
+
+    return 0;
+}
