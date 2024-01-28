67cc03e7c870afb2f354506de077fcf4c4cfd3f2
MS_D_rei <msmdmission1111@gmail.com>
2024-01-28 11:18:44 +0900

    Add: the length of std::array is constexpr and std::get function
    in chapter17/intro-array.cpp

--- chapter17/intro-array.cpp
+++ chapter17/intro-array.cpp
@@ -21,12 +21,25 @@ int main() {
     // `std::array` uses aggregate initialization.
     // it means that array doesn't have constructor.
     constexpr std::array<int, 8> primes{2, 3, 5, 7, 11, 13, 17, 19};
-    std::cout << "size: " << primes.size() << '\n';
+
+    // `std::array` has `size()` member function.
+    // it returns constexpr std::size_t.
+    constexpr int primesSize = primes.size();
+    std::cout << "size: " << primesSize << '\n';
 
     std::array<int, RGB::max_colors> rgb{};
     rgb[RGB::red] = 255;
     rgb[RGB::green] = 160;
     rgb[RGB::blue] = 0;
+
+    // `std::get` does complie-time bound check.
+    std::cout << "red: " << std::get<RGB::red>(rgb) << '\n';
+    std::cout << std::get<1>(primes) << '\n';
+    // compile error
+    // In template: static assertion failed due to requirement '9UL < 8UL':
+    // Index out of bounds in std::get<> (const std::array)
+    // [static_assert_requirement_failed]
+    // std::cout << std::get<9>(primes) << '\n';
 
     return 0;
 }
