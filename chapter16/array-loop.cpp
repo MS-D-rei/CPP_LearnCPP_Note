63f4d893f928113992b5edebf733400761a869ea
MS_D_rei <msmdmission1111@gmail.com>
2024-03-11 15:22:47 +0900

    Add: example for unsigned index and signed index for large array
    in chapter16/array-loop.cpp

--- chapter16/array-loop.cpp
+++ chapter16/array-loop.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <vector>
 
+using Index = std::ptrdiff_t;
+
 template <typename T>
 T calculateAverage(const std::vector<T>& vec) {
     T sum{};
@@ -11,10 +13,20 @@ T calculateAverage(const std::vector<T>& vec) {
     // Prefer to use signed type for index.
     // Pre C++20, we can use static_cast<int>(vec.size()) to convert.
     // From C++20, we can use std::ssize(vec) to get signed size.
+    //
+    // 2. Using unsigned type for index.
+    // for (typename std::vector<T>::size_type index{0}; index < vec.size(); ++index) {
+    //     sum += vec[index];
+    // }
+    // 3. Using signed type for index. (int)
     int vecLength{static_cast<int>(vec.size())};
     for (int index{0}; index < vecLength; ++index) {
         sum += vec[static_cast<std::size_t>(index)];
     }
+    // 4. Signed type for large array. (std::ptrdiff_t)
+    // for (Index index{0}; index < static_cast<Index>(vec.size()); ++index) {
+    //     sum += vec[static_cast<typename std::vector<T>::size_type>(index)];
+    // }
     return sum / static_cast<T>(vec.size());
 }
 
